706e935b77ddb24826e3550886382c3a893a2fec
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-09-04 12:58:18 -0700

    Update wit.rb
    added temporary name generator to wit.rb

--- wit.rb
+++ wit.rb
@@ -14,3 +14,53 @@ puts smack
 
 #stops the program from closing itself out, waiting for user to press enter.
 gets.chomp
+
+#generates a random name with many optional properties
+def genName(
+	syllCount = rand(2..3),
+	sym = false,
+	conWt = [],
+	vowWt = []
+	)
+	
+	temp = {}
+	temp[:rand] = rand(0..1)
+	temp[:syll] = syllCount
+	temp[:build] = []
+	
+	if temp[:rand] == 0
+		temp[:build].push("vow")
+		temp[:syll] -= 1
+	elsif temp[:rand] == 1
+		temp[:build].push("con")
+	end
+	
+	while temp[:syll] > 0
+		if temp[:build][-1] == "con"
+			temp[:build].push("vow")
+			temp[:syll] -= 1
+		elsif temp[:build][-1] == "vow"
+			temp[:build].push("con")
+		end
+	end
+	
+	temp[:rand] = rand(0..1)
+	
+	if temp[:rand] == 1
+		temp[:build].push("con")
+	end
+	
+	return temp[:build]
+	
+	#content here
+
+end
+
+loop do
+	b = gets.chomp.to_s
+	if b == ""
+		puts genName
+	else
+		break
+	end
+end
