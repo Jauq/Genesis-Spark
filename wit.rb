259a89095cb2c1f9576259f42199dd8ab2ca2e9e
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-09-04 10:44:31 -0700

    updated wit.rb and defs/test.rb

--- wit.rb
+++ wit.rb
@@ -1,6 +1,16 @@
-a = Dir.entries("defs")
-a = a[2..a.length - 1]
+#grabs all .rb files in the folder named "defs"
+a = Dir.glob("defs/*.rb")
 
-puts a
+print a
+puts ""
 
+#requires the code for files in the variable "a" so that we can use all of their definitions and classes
+a.each do |z|
+	require_relative "#{z}"
+end
+
+#test to see if definition "smack" from "test.rb" works.
+puts smack
+
+#stops the program from closing itself out, waiting for user to press enter.
 gets.chomp
